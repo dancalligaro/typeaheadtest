19ca89e7d644c7653160d55aa4fbaf7a0f1fc641
Daniel Calligaro <dancalligaro@gmail.com>
2015-03-18 00:10:53 -0300

    asd

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ app.controller('MainCtrl', function($scope) {
 
 });
 
-app.controller('TypeaheadCtrl', function($scope, $http) {
+app.controller('TypeaheadCtrl', function($scope, $http, $q, $timeout) {
 
   $scope.personsData = null;
 
@@ -12,6 +12,32 @@ app.controller('TypeaheadCtrl', function($scope, $http) {
   .then(function(res){ 
     $scope.personsData = res.data;
   })
+
+  var theTo;
+  var isResolved = true;
+  var theDeferred;
+
+  $scope.getPerson = function(val){
+    if(isResolved){
+      isResolved = false;
+      theDeferred = $q.defer();
+    }
+    $timeout.cancel(theTo);
+    theTo = $timeout(function(){ 
+      var ucaseFilter = val.toLowerCase();
+      isResolved = true;
+      var results = $scope.personsData.filter(function(item){ 
+        return item.name.toLowerCase().indexOf(ucaseFilter) > -1; 
+      });
+      theDeferred.resolve( results ); 
+    }, 300);
+    return theDeferred.promise;
+  };
+
+  $scope.oneSelected = function(){
+    console.log("Selected: ", $scope.asyncSelected2.name)
+    $timeout(function(){ $scope.asyncSelected2 = null; },500)
+  }
 
   // Any function returning a promise object can be used to load values asynchronously
   $scope.getLocation = function(val) {
